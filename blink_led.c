c9a1903e045da453ef0dbc68d61ad5b686533abe
MadhanM123 <madhangopalvel@gmail.com>
2023-09-12 17:22:47 -0700

    fixed make

--- blink_led.c
+++ blink_led.c
@@ -1,18 +1,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
  
-#define BLINK_INTERVAL 1000
+#define BLINK_INTERVAL 30
  
 int main (void) {
-    /* set pin 7 of PORTB for output*/
+
+    /* Sets Port B Pin 7 for output */
     DDRB = DDRB | (1 << DDB7);
     
     while(1) {
-        /* set pin 7 high to turn led on */
+
+        /* Set Port B Pin 7 to high to turn led on */
         PORTB = PORTB | (1 << PORTB7);
         _delay_ms(BLINK_INTERVAL);
         
-        /* set pin 7 low to turn led off */
+        /* Set Port B Pin 7 to low to turn led off */
         PORTB = PORTB & ~(1 << PORTB7);
         _delay_ms(BLINK_INTERVAL);
     }
